1163d08eaae0ac53f9e5edb2565b9990cb875a93
Rudolf Dittrich <rudi.dittrich77@gmail.com>
2017-02-26 11:42:11 +0100

    implemented all nodes, layers up to the MSE node

--- lesson7/miniflow/miniflow.py
+++ lesson7/miniflow/miniflow.py
@@ -3,6 +3,8 @@
 You need to change the Add() class below.
 """
 
+import numpy as np
+
 class Node(object):
     def __init__(self, inbound_nodes=[]):
         # Nodes from which this Node receives values
@@ -70,7 +72,92 @@ class Mult(Node):
         self.value = 1
         
         for node in self.inbound_nodes:
-            self.value *= node.value        
+            self.value *= node.value     
+            
+
+class Linear(Node):
+    def __init__(self, inputs, weights, bias):
+        Node.__init__(self, [inputs, weights, bias])
+
+        # NOTE: The weights and bias properties here are not
+        # numbers, but rather references to other nodes.
+        # The weight and bias values are stored within the
+        # respective nodes.
+
+    def forward(self):
+        """
+        Set self.value to the value of the linear function output.
+
+        Your code goes here!
+        """
+        X = np.array(self.inbound_nodes[0].value)
+        W = np.array(self.inbound_nodes[1].value)
+        b = self.inbound_nodes[2].value
+        
+        self.value = np.dot( X, W ) + b
+                           
+class Sigmoid(Node):
+    """
+    You need to fix the `_sigmoid` and `forward` methods.
+    """
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        """
+        This method is separate from `forward` because it
+        will be used later with `backward` as well.
+
+        `x`: A numpy array-like object.
+
+        Return the result of the sigmoid function.
+
+        Your code here!
+        """
+        return  1. / (1. + np.exp(-x))
+
+
+    def forward(self):
+        """
+        Set the value of this node to the result of the
+        sigmoid function, `_sigmoid`.
+
+        Your code here!
+        """
+        self.value = self._sigmoid(self.inbound_nodes[0].value)
+
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+        # TODO: your code here
+        
+        # MSE = 1/m * sum( (y - a)^2 )
+        # m ... number of examples
+        # y ... target values
+        # a ... computed values        
+        diff = y - a
+        self.value = np.mean(diff**2)      
 
 """
 No need to change anything below here!
@@ -119,7 +206,7 @@ def topological_sort(feed_dict):
     return L
 
 
-def forward_pass(output_node, sorted_nodes):
+def forward_pass(sorted_nodes):
     """
     Performs a forward pass through a list of sorted nodes.
 
@@ -134,6 +221,5 @@ def forward_pass(output_node, sorted_nodes):
     for n in sorted_nodes:
         n.forward()
 
-    return output_node.value
 
 
