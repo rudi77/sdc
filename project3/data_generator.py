9c33c4862e616aebc1a861e150b08e610352a93b
Rudolf Dittrich <rudi.dittrich77@gmail.com>
2017-04-02 20:46:26 +0200

    added data_generator and filled model.py

---
+++ project3/data_generator.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 20:22:00 2017
+
+@author: rudi
+"""
+import os
+import cv2
+import numpy as np
+import sklearn
+
+steering_angle_left_right = 0.061
+
+# Taken from Stackoverflow:
+# http://stackoverflow.com/questions/25699439/how-to-iterate-over-consecutive-chunks-of-pandas-dataframe-efficiently
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+def image_and_angle(sample, camera):
+    path = os.path.join('data', sample.iat[0,camera].strip())
+    image = cv2.imread(path)
+    angle = float(sample.iat[0,3])
+    
+    return (image, angle)
+
+
+def generator(samples, batch_size):
+    num_samples = len(samples)
+        
+    while 1: # Loop forever so the generator never terminates
+        samples = sklearn.utils.shuffle(samples)
+        for batch_samples in chunker(samples,batch_size):
+           
+            images = []
+            angles = []
+            
+            for batch_sample in chunker(batch_samples, 1):
+                # Center image
+                center_image, center_angle = image_and_angle(batch_sample, 0)
+                images.append(center_image)
+                angles.append(center_angle)
+            
+                # Left image
+                left_image, left_angle = image_and_angle(batch_sample, 1)
+                images.append(left_image)
+                angles.append(left_angle + steering_angle_left_right)
+                
+                # Right image
+                right_image, right_angle = image_and_angle(batch_sample, 2)
+                images.append(right_image)
+                angles.append(right_angle - steering_angle_left_right)
+        
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            
+            yield sklearn.utils.shuffle(X_train, y_train)
--- project3/model.py
+++ project3/model.py
@@ -4,13 +4,85 @@ Created on Thu Mar 30 21:27:38 2017
 
 @author: rudi
 """
-
-import os
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-
-# Initial Setup for Keras
+from keras.layers.core import K
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten
-from keras.layers.convolutional import Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers import Conv2D, MaxPooling2D, Input
+from keras.layers.pooling import AveragePooling2D
+from keras.layers import Cropping2D
+from keras.layers.core import Dropout
+from keras.layers.normalization import BatchNormalization
+
+def nvidia_net():
+    keep_prob = 0.5
+    keep_prob_fc = 0.5
+
+    model = Sequential()
+
+    # pre-processing
+    model.add(Cropping2D(cropping=((40,25), (0,0)), input_shape=(160,320,3)))
+    
+    model.add(Lambda(lambda x: K.tf.image.resize_images(x, (66,200)))) 
+    
+    model.add(Lambda(lambda x: (x - 128.0) / 128.0)) # normalization
+        
+    # Convnet
+    
+    model.add(Conv2D(24, 5, strides=(2,2), padding='valid', activation='relu'))
+    model.add(Dropout(keep_prob))
+
+    model.add(Conv2D(36, 5, strides=(2,2), padding='valid', activation='relu'))
+    model.add(Dropout(keep_prob))
+
+    model.add(Conv2D(48, 5, strides=(2,2), padding='valid', activation='relu'))
+    model.add(Dropout(keep_prob))
+
+    model.add(Conv2D(64, 3, activation='relu'))
+    model.add(Dropout(keep_prob))
+
+    model.add(Conv2D(64, 3, activation='relu'))
+    model.add(Dropout(keep_prob))
+
+    # FC
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dropout(keep_prob_fc))
+    model.add(Dense(50))
+    model.add(Dropout(keep_prob_fc))
+    model.add(Dense(10))
+    model.add(Dropout(keep_prob_fc))
+    model.add(Dense(1))
+
+    return model
+
+import pandas as pd
+import data_generator as dg
+df = pd.read_csv('data/driving_log.csv')
+
+# Split samples into training and validation samples.
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(df, test_size=0.2)
+
+train_generator = dg.generator(train_samples, batch_size=32)
+validation_generator = dg.generator(validation_samples, batch_size=32)
+
+
+model_n = nvidia_net()
+model_n.summary()
+
+# Mean square error function is used as loss function because this 
+# is a regression problem.
+model_n.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+# fit_generator(self, generator, steps_per_epoch, epochs=1, verbose=1, callbacks=None, 
+#     validation_data=None, validation_steps=None, class_weight=None, 
+#     max_q_size=10, workers=1, pickle_safe=False, initial_epoch=0)
+
+model_n.fit_generator(train_generator, 
+                      steps_per_epoch = len(train_samples),
+                      epochs = 1,
+                      validation_data=validation_generator,
+                      validation_steps=len(validation_samples))
+
+# save model
+model_n.save('./model.h5')
